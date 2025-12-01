23da4374a9c5440ec30b15aba0550c637dbef0a8
miladzazai <miladzazai@cmail.carleton.ca>
2025-12-01 01:05:32 -0500

    new test

--- src/test/java/com/trustworthyreviews/controller/UserControllerIntegrationTest.java
+++ src/test/java/com/trustworthyreviews/controller/UserControllerIntegrationTest.java
@@ -3,11 +3,14 @@ import com.trustworthyreviews.TestSecurityConfig;
 
 
 import com.trustworthyreviews.security.SupabaseUser;
+import com.trustworthyreviews.service.UserService;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -30,6 +33,9 @@ public class UserControllerIntegrationTest {
 
    @Autowired
    private MockMvc mockMvc;
+   
+   @MockBean
+    private UserService userService; 
 
 
    @AfterEach
